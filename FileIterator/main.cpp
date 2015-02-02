e1eebeb500c4dfbde5708f5c53ff42c9307e96e1
Thorsten Flügel <thorsten.fluegel@tu-dortmund.de>
2015-02-02 20:56:31 +0100

    list all files in all folders passed via command line

--- FileIterator/main.cpp
+++ FileIterator/main.cpp
@@ -1,6 +1,48 @@
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <algorithm>
+#include <iterator>
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <string>
+#include <list>
+
+
+std::list<std::wstring> filesInFolder(const std::wstring& path)
+{
+	std::list<std::wstring> files;
+
+	std::wstring searchMask(path + L"\\*");
+	WIN32_FIND_DATA findData;
+	HANDLE findHandle = FindFirstFile(searchMask.c_str(), &findData);
+	while (findHandle != INVALID_HANDLE_VALUE)
+	{
+		if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY &&
+			std::wstring(L".") != findData.cFileName &&
+			std::wstring(L"..") != findData.cFileName)
+		{
+			std::wstring dir(path + L"\\" + findData.cFileName);
+			auto folder_content = filesInFolder(dir);
+			std::copy(folder_content.cbegin(), folder_content.cend(), std::back_inserter(files));
+		}
+		else if (!(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+		{
+			std::wstring filepath(path + L"\\" + findData.cFileName);
+			files.push_back(filepath);
+		}
+
+		if (!FindNextFile(findHandle, &findData))
+		{
+			findHandle = INVALID_HANDLE_VALUE;
+		}
+	}
+	FindClose(findHandle);
+
+	return files;
+}
 
 
 std::chrono::duration<double> time(std::function<void ()> f)
@@ -22,5 +64,23 @@ void wmain(int argc, wchar_t** argv)
 		std::this_thread::sleep_for(std::chrono::seconds(1));
 	};
 
+	std::list<std::wstring> files;
+	for (auto i = 1; i < argc; ++i)
+	{
+		auto _files = filesInFolder(argv[i]);
+		for (auto& f : _files)
+		{
+			files.push_back(f);
+		}
+	}
+
+	auto list_files = [](const std::list<std::wstring>& file_list) {
+		for (auto& f : file_list)
+		{
+			std::wcout << f << std::endl;
+		}
+	};
+
 	std::wcout << L"slept for " << time(sleep_1s).count() << L"s" << std::endl;
+	std::wcout << L"listing files took " << time(std::bind(list_files, files)).count() << L"s" << std::endl;
 }
